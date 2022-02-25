3dc9257687af3a791824dc9218a44127be3db39a
n3cr0Tech <ztxter@gmail.com>
2022-02-24 21:46:16 -0800

    Fixed the automation scripts for setup and running the time_write.py

--- automation_script.sh
+++ automation_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Bluetooth Time & Date Update Automation Script STARTED"
 echo "(Make sure you updated the active directories in this script)"
-source "./bluetooth_project/dev/bin/activate"
-python3 bluetooth_project/write_time.py
+source "./dev/bin/activate"
+python3 ./time_write.py
---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo "creating virtualenv"
+python3 -m venv dev
+echo "installing stuff..."
+pip3 install -r requirements.txt
